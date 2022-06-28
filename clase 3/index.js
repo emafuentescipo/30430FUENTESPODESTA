44d47b55416febbdcd0b50cf3094825b752c6c16
Emmanuel Fuentes Podesta <emafuentescipo@hotmail.com>
2022-06-28 17:46:45 -0300

    inicio version de java

---
+++ clase 3/index.js
@@ -0,0 +1,6 @@
+let usuario = prompt("Ingrese un usuario").toLowerCase();
+while(usuario != "emmanuel"){
+  alert("Usuario incorrecto");
+  usuario = prompt("Ingrese un usuario");
+}
+alert("Bienvenido");
---
+++ clase2/index.js
@@ -0,0 +1,10 @@
+let edad = 21;
+if(edad < 16){
+  console.log("bienvenido a la fiesta con un adulto responsable");
+}else if(edad < 18){
+  console.log("bienvenido a la fiesta con un amigo");
+}else if(edad === 20){
+  console.log("Puedes entrar solo a la fiesta");
+}else{
+  console.log("no puedes entrar a la fiesta");
+}
